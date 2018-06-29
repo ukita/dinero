caa68851dacbf1081e41128e532f23025349f77c
Bruno Ukita <brunoukita@gmail.com>
2018-06-29 19:57:31 -0300

    add wallet queries

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,25 @@
 import { getUserId, Context } from '../utils'
 
 export const Query = {
-
   me: (parent, args, ctx: Context, info) => {
     const userId = getUserId(ctx)
     return ctx.db.query.user({ where: { id: userId } }, info)
+  },
+
+  wallets: (parent, args, ctx: Context, info) => {
+    const userId = getUserId(ctx)
+    return ctx.db.query.wallets({
+      where: { user: { id: userId } }
+    }, info)
+  },
+
+  wallet: async (parent, { id }, ctx: Context, info) => {
+    const userId = getUserId(ctx)
+
+    const [wallet] = await ctx.db.query.wallets({
+      where: { id, user: { id: userId } }
+    }, info)
+
+    return wallet
   }
 }
