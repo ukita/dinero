831eeddfde13a5fa855c583ed766a839d9fdb5e0
Bruno Ukita <brunoukita@gmail.com>
2019-05-02 17:37:55 +0100

    add permissions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { prisma } from './generated/prisma-client'
 import { resolvers } from './resolvers'
+import { permissions } from './permissions'
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  middlewares: [permissions],
   context: request => ({
     ...request,
     prisma
---
+++ backend/src/permissions/index.ts
@@ -0,0 +1,29 @@
+import { rule, shield, and } from 'graphql-shield'
+import { getUserId, Context } from '../utils'
+
+const rules = {
+  isAuthenticatedUser: rule()((parent, args, context: Context) => {
+    const userId = getUserId(context)
+
+    return context.prisma.$exists.user({
+      id: userId
+    })
+  }),
+
+  canAddTransaction: rule()(async (parent, { walletId }, context: Context) => {
+    const userId = getUserId(context)
+    const user = await context.prisma.wallet({ id: walletId }).user()
+
+    return userId === user.id
+  })
+}
+
+export const permissions = shield({
+  Query: {
+    viewer: rules.isAuthenticatedUser
+  },
+  Mutation: {
+    addWallet: rules.isAuthenticatedUser,
+    addTransaction: and(rules.isAuthenticatedUser, rules.canAddTransaction)
+  }
+})
