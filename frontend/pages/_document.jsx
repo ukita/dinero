00eb590b35184b8ae68a33e47b4d949121d7bb1e
Bruno Ukita <brunoukita@gmail.com>
2019-06-14 15:54:33 +0100

    add lang attr to html

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -17,5 +17,17 @@ export default class MyDocument extends Document {
       ...initialProps,
       styles: [...initialProps.styles, ...sheet.getStyleElement()]
     };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
