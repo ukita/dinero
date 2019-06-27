c03c7f6d7dfcf792b7d233158e28f38c5888ba8a
Bruno Ukita <brunoukita@gmail.com>
2019-06-27 17:56:47 +0100

    Transaction card

--- frontend/components/Dashboard/Wallet.jsx
+++ frontend/components/Dashboard/Wallet.jsx
@@ -5,7 +5,8 @@ import WalletQuery from "@components/Wallet";
 import { Text, Heading, Paragraph } from "@components/Typography";
 import Card from "@components/Card";
 import { getProp } from "@lib/utils";
-import { Box } from "@components/Layout";
+import { Box, Flex } from "@components/Layout";
+import { ShoppingBag, LogIn } from "react-feather";
 
 function Wallet({ walletId = "" }) {
   return (
@@ -15,18 +16,51 @@ function Wallet({ walletId = "" }) {
 
         if (wallet) {
           return (
-            <Box>
-              <Box p={3}>
+            <Box p={3}>
+              <Box>
                 <Heading>{wallet.name}</Heading>
                 <Paragraph>{wallet.description}</Paragraph>
               </Box>
-              <Box mt={2}>
-                <Card boxShadowSize="md" p={3}>
-                  <Text>Transaction #1</Text>
-                </Card>
-                <Card boxShadowSize="md" p={3} mt={3}>
-                  <Text>Transaction #1</Text>
-                </Card>
+              <Box mt={4}>
+                <Heading fontSize={3}>Transactions</Heading>
+                <Box mt={3}>
+                  <Card boxShadowSize="md" p={3}>
+                    <Flex>
+                      <Flex alignItems="center" flexGrow={1}>
+                        <ShoppingBag />
+                        <Box ml={3}>
+                          <Text as="b">Transaction #1</Text>
+                          <Paragraph fontSize={1} color="gray">
+                            10/10/2019 10:30
+                          </Paragraph>
+                        </Box>
+                      </Flex>
+                      <Flex>
+                        <Text as="b" color="red.4">
+                          - $125.50
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </Card>
+                  <Card boxShadowSize="md" p={3} mt={3}>
+                    <Flex>
+                      <Flex alignItems="center" flexGrow={1}>
+                        <LogIn />
+                        <Box ml={3}>
+                          <Text as="b">Transaction #2</Text>
+                          <Paragraph fontSize={1} color="gray">
+                            10/10/2019 10:40
+                          </Paragraph>
+                        </Box>
+                      </Flex>
+                      <Flex>
+                        <Text as="b" color="teal.4">
+                          $50
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  </Card>
+                </Box>
               </Box>
             </Box>
           );
